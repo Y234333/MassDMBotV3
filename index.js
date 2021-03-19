2ff3847b53f97e327dea1bf72fcc5eb8e5c3c9aa
Y234333 <tousefhi@gmail.com>
2021-03-19 23:00:58 +0300

    Initial

---
+++ index.js
@@ -0,0 +1,36 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "!";
+
+client.on("message", message => {
+
+if (message.content.startsWith(prefix + "dm")) {
+if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+let args = message.content.split(" ").slice(1);
+var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` **MESSAGE SENT** `); 
+ message.delete(); 
+};     
+});
+
+
+client.on('ready',()=>{
+    console.log(' Bot is online now!');
+    let statuses = [
+        " Y234333 is OP ",
+        
+    ]
+    setInterval(function(){
+            let status = statuses[Math.floor(Math.random() * statuses.length)];
+            client.user.setActivity(status, {type:"WATCHING"})
+    
+        }, 3000) //Seconds to Random
+
+
+
+});
+
+client.login("NjI3MTA2NjkwODM1MTUyOTA3.XY30ng.eyLbuzyiu_W_ibhVa7gN0o8o6fA");
